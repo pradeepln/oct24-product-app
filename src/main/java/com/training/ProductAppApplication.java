614845e72784bb782ca7095151905f63be1d9f6a
Pradeep LN <pradeep.ln78@gmail.com>
2024-10-14 17:07:38 +0530

    after day1 Spring as DI Container

---
+++ src/main/java/com/training/ProductAppApplication.java
@@ -0,0 +1,26 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.training.ui.ProductConsoleUI;
+
+@SpringBootApplication
+public class ProductAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext springContainer = 
+					SpringApplication.run(ProductAppApplication.class, args);
+		
+		System.out.println("-------------- Creattion of ApplicationContext is now complete -------------");
+		
+		ProductConsoleUI ui = springContainer.getBean(ProductConsoleUI.class); //new ProductConsoleUI();
+		ProductConsoleUI ui2 = springContainer.getBean(ProductConsoleUI.class);
+		
+		System.out.println("Are they same obj? "+(ui == ui2));
+		
+		ui.createProductWithUI();
+	}
+
+}
---
+++ src/main/java/com/training/ui/ProductConsoleUI.java
@@ -0,0 +1,45 @@
+package com.training.ui;
+
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.training.domain.Product;
+import com.training.service.ProductService;
+import com.training.service.ProductServiceImpl;
+
+@Component
+public class ProductConsoleUI {
+	
+	ProductService service; // = new ProductServiceImpl();
+	
+	public ProductConsoleUI() {
+		System.out.println("----->>>> ProductConsoleUI constructor");
+	}
+	
+	@Autowired
+	public void setService(ProductService service) {
+		System.out.println("----->>>> Injecting service into ui");
+		this.service = service;
+	}
+	
+	public void createProductWithUI() {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Enter a name:");
+		String name = kb.nextLine();
+		System.out.println("Enter a price:");
+		float price = Float.parseFloat(kb.nextLine());
+		System.out.println("Enter a QoH:");
+		int qoh = Integer.parseInt(kb.nextLine());
+		
+		Product p = new Product(name, price, qoh);
+		int id = service.addProduct(p);
+		
+		System.out.println("Created Product with ID: "+id);
+		
+	}
+
+}
